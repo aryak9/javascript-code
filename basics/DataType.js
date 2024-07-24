166c1bdaa136d50c9c5b649301d8aa1d3e4fe3f7
aryak9 <108917223+aryak9@users.noreply.github.com>
2024-07-24 05:27:31 +0000

    DataType

---
+++ basics/DataType.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version 
+
+ //alert(3+3) //we are using nodejs, not browser
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+//boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+//object
+
+console.log(typeof "125"); 
+console.log(typeof 125);
+
+
+// null output is a object
+
+
+
+
+
+
+
