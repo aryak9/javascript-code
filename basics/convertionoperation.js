f711574ec07310c683651b278282c9d1402d1d83
aryak9 <108917223+aryak9@users.noreply.github.com>
2024-07-24 05:52:41 +0000

    commit

---
+++ basics/convertionoperation.js
@@ -0,0 +1,21 @@
+let score = 45
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isloggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+// "" => false
+// "hitesh" => true
